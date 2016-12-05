acdaa61cf3ce6bfa66498f5342834164957821f4
Justin Mitchell <jmitchell38488@gmail.com>
2016-12-05 11:32:59 +1100

    Moved the routing configuration for the app.phones module into the app.phones module from the app config, so that it is correctly containerised and the base application doesn't know about it

--- src/app/js/app.config.js
+++ src/app/js/app.config.js
@@ -1,15 +1,4 @@
 angular.module('app')
     .config(function($routeProvider) {
         'use strict';
-
-        $routeProvider
-            .when('/', {
-                template: function() {
-                    return '<phone-list></phone-list>'
-                },
-                controller: 'app.phones.PhoneListController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
     });
--- src/app/js/components/app.phones/module.js
+++ src/app/js/components/app.phones/module.js
@@ -1,3 +1,26 @@
 'use strict';
 
-angular.module('app.phones', ['ngResource']);
+angular.module('app.phones', ['ngResource', 'ngRoute'])
+       .config([
+           '$locationProvider', '$routeProvider',
+           function ($locationProvider, $routeProvider) {
+               $locationProvider.hashPrefix('!');
+
+               $routeProvider
+                   .when('/phones', {
+                       template: function() {
+                           return '<phone-list></phone-list>'
+                       },
+                       controller: 'app.phones.PhoneListController'
+                   })
+                   .when('/phones/:phoneId', {
+                       template: function() {
+                           return '<phone-details></phone-details>'
+                       },
+                       controller: 'app.phones.PhoneDetailsController'
+                   })
+                   .otherwise({
+                       redirectTo: '/phones'
+                   });
+           }
+       ]);
